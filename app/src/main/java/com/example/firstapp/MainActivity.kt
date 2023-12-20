dea4565e2b950d373f07302f973731c4f47f4c37
Daniil Ivantsov <kasta0071@mail.ru>
2023-12-20 15:17:53 +0300

    HW 3

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,43 +1,44 @@
 package com.example.firstapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.CheckBox
 import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fio: EditText
+    private lateinit var iphone: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val submit: Button = findViewById(R.id.submit)
-        val h1: TextView = findViewById(R.id.h1)
-        val email: EditText = findViewById(R.id.email)
-        val mail: ImageView = findViewById(R.id.mail)
-        val password: EditText = findViewById(R.id.password)
-        val lock: ImageView = findViewById(R.id.lock)
-        val enter: TextView = findViewById(R.id.enter)
-        val description: TextView = findViewById(R.id.description)
-        val services: LinearLayout = findViewById(R.id.services)
-        val check: CheckBox = findViewById(R.id.lisence)
+        val phone: Button = findViewById(R.id.phone)
+        fio = findViewById(R.id.email)
+        iphone = findViewById(R.id.password)
 
-        submit.setOnClickListener{
-            h1.visibility = View.GONE
-            email.visibility = View.GONE
-            mail.visibility = View.GONE
-            password.visibility = View.GONE
-            lock.visibility = View.GONE
-            check.visibility = View.GONE
-            enter.visibility = View.VISIBLE
-            description.visibility = View.VISIBLE
-            services.visibility = View.VISIBLE
-            submit.setText(R.string.exit)
+        submit.setOnClickListener {
+            startSecond()
         }
 
+        phone.setOnClickListener {
+            dialNumber()
+        }
     }
+
+    private fun startSecond() {
+        intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("FIO", fio.text.toString())
+        startActivity(intent)
+    }
+
+    private fun dialNumber() {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:" + iphone.text.toString())
+        }
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/firstapp/SecondActivity.kt
@@ -0,0 +1,47 @@
+package com.example.firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity()  {
+    private lateinit var ret: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val submit: Button = findViewById(R.id.retur)
+        ret = findViewById(R.id.new_text)
+
+        ret.text = intent.getStringExtra("FIO")
+
+        submit.setOnClickListener {
+            startFirst()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVED_STRING, ret.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val saved = savedInstanceState.getString(SAVED_STRING)
+        ret.text = saved
+
+        println("Restore First")
+    }
+
+    private fun startFirst() {
+        startActivity(
+            Intent(this, MainActivity::class.java)
+        )
+    }
+
+    companion object {
+        private const val SAVED_STRING = "SAVED_STRING"
+    }
+}
